ccc8996f05a65c6ff9023debfca53230fe0a120a
yuhui7red <yuhui7red@gmail.com>
2014-01-11 22:36:35 +0800

    modify

--- NewMapReduceSimulation/TestCaseAssign.m
+++ NewMapReduceSimulation/TestCaseAssign.m
@@ -3,10 +3,10 @@
 % [k3, total_max3, position3] = MultiHostOptimize(100000, 20, 245, server_info_001, vm_info_001, 100, 50, 80, 3, 2);
 % [k4, total_max4, position4] = MultiHostOptimize(100000, 20, 245, server_info_001, vm_info_001, 100, 50, 80, 4, 2);
 
-[k1, total_max1, position1] = MultiHostOptimize(200000, 100, 1225, server_info_002, vm_info_002, 100, 5, 400, 1, 1);
-[k2, total_max2, position2] = MultiHostOptimize(200000, 100, 1225, server_info_002, vm_info_002, 100, 5, 400, 2, 2);
-[k3, total_max3, position3] = MultiHostOptimize(200000, 100, 1225, server_info_002, vm_info_002, 100, 5, 400, 3, 2);
-[k4, total_max4, position4] = MultiHostOptimize(200000, 100, 1225, server_info_002, vm_info_002, 100, 5, 400, 4, 2);
+[k1, total_max1, position1] = MultiHostOptimize(270000, 100, 1225, server_info_002, vm_info_002, 100, 5, 700, 1, 1);
+[k2, total_max2, position2] = MultiHostOptimize(270000, 100, 1225, server_info_002, vm_info_002, 100, 5, 700, 2, 2);
+[k3, total_max3, position3] = MultiHostOptimize(270000, 100, 1225, server_info_002, vm_info_002, 100, 5, 700, 3, 2);
+[k4, total_max4, position4] = MultiHostOptimize(270000, 100, 1225, server_info_002, vm_info_002, 100, 5, 700, 4, 2);
 % plot(total_max1(:, 1), total_max1(:, 2), 'ro');
 hold on;
 plot(total_max1(:, 1), total_max1(:, 2), 'r');
