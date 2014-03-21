daa18ff3c9834b09f9bae147b1981336fd3b61d6
yuhui7red <yuhui7red@gmail.com>
2014-03-21 15:11:37 +0800

    modify

--- MultiHost/Evaluation/Test.py
+++ MultiHost/Evaluation/Test.py
@@ -1,1 +1,23 @@
+import os, sys, string
+import MySQLdb
 
+try:
+    conn = MySQLdb.connect(host = '166.111.143.250', user = 'root', passwd = 'cer.cloud', db = 'nova')
+    
+except Except, e:
+    print e
+    sys.exit()
+
+sql = 'select hypervisor_hostname,vcpus,memory_mb from compute_nodes'
+cursor = conn.cursor()
+cursor.execute(sql)
+
+data = cursor.fetchall()
+
+if data:
+    for element in data:
+        print element[0]
+
+cursor.close()
+
+conn.close()
