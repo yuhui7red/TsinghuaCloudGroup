5b25abf6c2aaf9989c974b06fe553173a08e9b4b
yuhui7red <yuhui7red@gmail.com>
2014-04-19 22:41:04 +0800

    almost finish

--- MultiHost/Controller/Main.py
+++ MultiHost/Controller/Main.py
@@ -7,6 +7,10 @@ if not "../ComputeConstruction/" in sys.path:
     sys.path.append("../ComputeConstruction/")
 if not "../Evaluation/" in sys.path:
     sys.path.append("../Evaluation/")
+if not "../DeployStrategy/" in sys.path:
+    sys.path.append("../DeployStrategy")
+if not "../AssignStrategy/" in sys.path:
+    sys.path.append("../AssignStrategy/")
 
 import os, urllib, httplib, json, base64, string
 from urlparse import urlparse
@@ -15,6 +19,9 @@ from Authentication import Authenticate
 from ComputeNodes import GetComputeNodesInfo
 from VMCapabilityEvaluation import VMCapabilityEvaluation
 from Flavors import ListFlavors, GetFlavors
+from Optimal import GetOptimalAgentNumber, GetOptimalNa
+from NewKnapsack import NewKnapsack
+from LoadBalancing import LoadBalancing
 
 authentication = Authenticate('166.111.143.250', 'admin', 'admin', 'cer.cloud')
 apiToken = APIToken()
@@ -45,15 +52,98 @@ for i in range(serverNumber):
     for r in range(vm[i]):
         vmInfo[VMCount-1][0] = VMCount
         vmInfo[VMCount-1][1] = serverInfo[i][0]
-        vmInfo[VMCount-1][2] = VMCapabilityEvaluation(serverInfo[i][2], 1.634394, 0.855, serverInfo[i][1], serverInfo[i][3])
+        vmInfo[VMCount-1][2] = VMCapabilityEvaluation(serverInfo[i][2], 1.634394, serverInfo[i][1])
         VMCount = VMCount + 1
 
+'''for element in vmInfo:
+    print element
+
+for element in serverInfo:
+    print element'''
+
+#k = GetOptimalAgentNumber(N, S, nr, Sr, u0, r, B)
+#Na = GetOptimalNa(N, S, nr, Sr, u0, r, B, k)
+k = 3
+Na = 14
+
+position = NewKnapsack(vm, k, Na)
+routerPositions = []
+i = 1
+for element in position:
+    serverInfo[element-1][3] = 'router' + str(i)
+    i = i + 1
+    routerPositions.append(serverInfo[element-1][0])
+
+routerPositions.sort()
+
+vmInfo = sorted(vmInfo, key=lambda d:d[1])
+#serverInfo = sorted(serverInfo, key=lambda d:d[0])
+
+'''
 for element in vmInfo:
     print element
 
 for element in serverInfo:
     print element
+'''
+
+agentServerInfo = []
+for element in serverInfo:
+    if element[3] != 0:
+        agentServerInfo.append(element)
+
+agentServerInfo = sorted(agentServerInfo, key=lambda d:d[0])
+
+i = 0
+for j in range(len(vmInfo)):
+    if i == len(agentServerInfo):
+        break
+    if vmInfo[j][1] == agentServerInfo[i][0]:
+        vmInfo[j][3] = agentServerInfo[i][3]
+        if vmInfo[j+1][1] > agentServerInfo[i][0]:
+            i = i + 1
+
+decreaseRate = 0.855
+restVM = []
+for element in vmInfo:
+    if element[3] != 0:
+        element[2] = element[2] * decreaseRate
+    else:
+        restVM.append([element[0], element[2]])
 
 
+mapping = LoadBalancing(restVM, k)
+#print mapping
 
+vmInfo = sorted(vmInfo, key=lambda d:d[0])
 
+i = 0
+for element in vmInfo:
+    if i == len(mapping):
+        break
+    if mapping[i][0] == element[0]:
+        element[3] = 'router' + str(mapping[i][1])
+        i = i + 1
+
+'''for element in mapping:
+    print element
+
+for element in vmInfo:
+    print element'''
+
+routerNumbers = k
+# routerPositions
+# vm
+for elem in vmInfo:
+    print elem
+relations = []
+vmPosition = []
+
+for element in vmInfo:
+    relations.append(element[1])
+    vmPosition.append(element[3])
+
+print routerNumbers
+print routerPositions
+print relations
+print vmPosition
