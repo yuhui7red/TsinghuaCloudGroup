0b8cff2e6f48a92cf66a46fc0987c380b6ea4d4a
yuhui7red <yuhui7red@gmail.com>
2014-07-20 13:54:02 +0800

    modify

---
+++ INFOCOM2014/Poisson.m
@@ -0,0 +1,17 @@
+function [ArrivalTime] = Poisson(ArrivalTimeExpectation)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Poisson.m
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+     % Author: William Yu
+     % Date: 2014/7/20
+     % Revisor:
+     % Date:
+     % Function: 1.***; 2.***;
+     % Input: 1.***; 2.***;
+     % Output: 1.***; 2.***;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ArrivalTime = exprnd(ArrivalTimeExpectation);
+
+end
+
